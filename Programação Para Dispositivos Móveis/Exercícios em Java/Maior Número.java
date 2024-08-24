2825e4a7789b4a50e69def6f8719510e981d064f
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-08-24 15:04:56 -0300

    Atualizando.

---
+++ Programação Para Dispositivos Móveis/Exercícios em Java/Maior Número.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Exe2 {
+
+    public static void main(String[] args) {
+
+        Scanner MaiorValor = new Scanner(System.in);
+        int Valor = Integer.MIN_VALUE;
+
+        System.out.println("Informe 5 números, por gentileza: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Número " + (i + 1) + ": ");
+            int num = MaiorValor.nextInt();
+
+
+            if (num > Valor) {
+                Valor = num;
+
+
+            }
+
+            System.out.println("O maior número é: " + Valor);
+
+
+
+
+        }
+    }
+}
---
+++ Programação Para Dispositivos Móveis/Exercícios em Java/Par Ou Impar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exe1 {
+
+    public static void main (String[] args){
+
+        Scanner ParOuImpar = new Scanner(System.in);
+
+
+        // Informando o número correspondente:
+        System.out.println("Informe um número, por gentileza: ");
+        int num = ParOuImpar.nextInt();
+// _____________________________________________________________________________________________________________________
+
+        // Se o número informado for divísivel por 2 ele é par. Caso contrário ele será impar...
+        if(num % 2 == 0){
+            System.out.println("O número que você descreveu, cujo é " + num + " é classificado como par!");
+        }else{
+            System.out.println("O número que você descreveu, cujo é " + num + " é classificado como ímpar!");
+
+        }
+
+
+
+    }
+
+
+}
