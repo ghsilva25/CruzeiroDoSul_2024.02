1e687f5dfee43252dcfa6f7a3a36f46f56834c1c
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-09-07 21:10:42 -0300

    Atualizando

---
+++ Programação Para Dispositivos Móveis/Exercícios em Java/SomaDosNum.java
@@ -0,0 +1,14 @@
+public class SomaDosNum {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 175;
+
+        // Número total de termos
+        int n = (b - a + 1);
+
+        // Soma dos números de a até b
+        int soma = (n * (a + b)) / 2;
+
+        System.out.println("A soma dos números de " + a + " a " + b + " é: " + soma);
+    }
+}
---
+++ Programação Para Dispositivos Móveis/Exercícios em Java/Tabuada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TabuadaUsuario {
+    public static void main(String[] args) {
+        // Criar um objeto Scanner para ler a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar ao usuário que insira um número
+        System.out.print("Digite um número para exibir a tabuada: ");
+        int numero = scanner.nextInt();
+
+        // Exibir a tabuada do número fornecido
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        // Fechar o scanner
+        scanner.close();
+    }
+}
